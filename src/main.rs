38bce8fbd8d85dc1204ce7747d859ba9a9bac30a
saito-sota-ab <saito_sota@applibot.co.jp>
2022-07-28 01:27:19 +0900

    main.rs の骨組み作成

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,51 @@
+use std::env;
+use std::env::args;
+use std::io::stderr;
+use log::error;
+
 fn main() {
-    println!("Hello, world!");
+    env::set_var("RUST_LOG", "debug");
+    env_logger::init();
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 4 {
+        error!("Please specify [tcp|udp] [server|client|] [addr:port].");
+        std::process::exit(1);
+    }
+
+    let protocol: &str = &args[1];
+    let role: &str = &args[2];
+    let address = &args[3];
+    match protocol {
+        "tcp" => match role {
+            "server" => {
+                // TODO: TCPサーバーの呼び出し
+            }
+            "client" => {
+                // TODO: TCPクライアントの呼び出し
+            }
+            _ => {
+                missing_role();
+            }
+        }
+        "udp" => match role {
+            "server" => {
+                // TODO: UDPサーバーの呼び出し
+            }
+            "client" => {
+                // TODO: UDPクライアントの呼び出し
+            }
+            _ => {
+                missing_role();
+            }
+        }
+        _ => {
+            error!("Please specify tcp or udp on the 1st argument.");
+            std::process::exit(1);
+        }
+    }
+}
+
+fn missing_role() {
+    error!("Please specify server or client on the 2nd argument.");
+    std::process::exit(1);
 }
