e40a82bf8149469de2ac0312fdbf1e6693ac9be9
saito-sota-ab <saito_sota@applibot.co.jp>
2022-07-28 00:50:10 +0900

    dependencies

---
+++ target/rls/debug/deps/socket_programming-44f295056323a2ca.d
@@ -0,0 +1,5 @@
+/Users/s10974/src/socket-programming/target/rls/debug/deps/socket_programming-44f295056323a2ca.rmeta: src/main.rs
+
+/Users/s10974/src/socket-programming/target/rls/debug/deps/socket_programming-44f295056323a2ca.d: src/main.rs
+
+src/main.rs:
---
+++ target/rls/debug/deps/socket_programming-b6c2c48b41d4835e.d
@@ -0,0 +1,5 @@
+/Users/s10974/src/socket-programming/target/rls/debug/deps/socket_programming-b6c2c48b41d4835e.rmeta: src/main.rs
+
+/Users/s10974/src/socket-programming/target/rls/debug/deps/socket_programming-b6c2c48b41d4835e.d: src/main.rs
+
+src/main.rs:
